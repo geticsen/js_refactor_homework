0edd35e8c837cbcbf96703c4b7c90b236c8b9c29
ZHOUGR2 <286348794@qq.com>
2020-08-27 21:42:43 +0800

    [deliveryTest] [Green] test: add test case 3:isRush true ,deliveryState in ELSE

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -13,7 +13,7 @@ deliveryTest('case 1:isRush true ,deliveryState in MA',t => {
     };
     t.is(2,deliveryDate(anOrder,true))
 })
-deliveryTest('case 1:isRush true ,deliveryState in NY',t => {
+deliveryTest('case 2:isRush true ,deliveryState in NY',t => {
     const anOrder={
         deliveryState:'NY',
         placedOn:{
@@ -23,4 +23,15 @@ deliveryTest('case 1:isRush true ,deliveryState in NY',t => {
         }
     };
     t.is(3,deliveryDate(anOrder,true))
+})
+deliveryTest('case 3:isRush true ,deliveryState in ELSE',t => {
+    const anOrder={
+        deliveryState:'ELSE',
+        placedOn:{
+            plusDays:(plusTime)=>{
+                return plusTime;
+            }
+        }
+    };
+    t.is(4,deliveryDate(anOrder,true))
 })

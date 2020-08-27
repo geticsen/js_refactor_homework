02706d6dcbe42f2b5f664365cd952c237e5c36b1
ZHOUGR2 <286348794@qq.com>
2020-08-27 21:54:12 +0800

    [deliveryTest] [Green] refactor:extract calculateRushdeliveryDate

--- src/delivery.js
+++ src/delivery.js
@@ -1,23 +1,25 @@
+calculateRushdeliveryDate = (anOrder) => {
+  if ([
+    'MA',
+    'CT',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  }
+  else if ([
+    'NY',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else {
+    deliveryTime = 3;
+  }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
 function deliveryDate(anOrder, isRush) {
   let deliveryTime;
   if (isRush) {
-    
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    return calculateRushdeliveryDate(anOrder);
   }
   else {
     if ([

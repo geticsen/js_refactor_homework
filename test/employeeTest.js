705ce40500b419351f02d3aaf80042ec7e34d7c8
ZHOUGR2 <286348794@qq.com>
2020-08-27 22:23:35 +0800

    [deliveryTest] [Green] test: add test case 2:validateType is not allow

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -6,3 +6,11 @@ employeeTest('test case 1:  Employee toString with name is jack and type is engi
     const employee =new Employee("jack","engineer");
     t.is("jack (engineer)",employee.toString())
 })
+employeeTest('test case 2: validateType is not allow',t => {
+    try{
+        new Employee("james","6666");
+        t.fail();
+    }catch(e){
+        t.is('Employee cannot be of type 6666',e.message)
+    }
+})

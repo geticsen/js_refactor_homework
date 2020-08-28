42d600eb1691d78faabd839b0170731d9a3cafca
ZHOUGR2 <286348794@qq.com>
2020-08-28 12:26:00 +0800

    [deliveryTest] [Green] test : add test 9

--- test/rankTest.js
+++ test/rankTest.js
@@ -150,3 +150,12 @@ rankTest('case 8 : voyage length is 19 zone is china  and history have china his
   const rank = rating(voyage,history.slice(0,16))
   t.is('A',rank)
 });
+rankTest('case 9 : voyage length is 10 zone is china  and history have china history length is 16', t => {
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+
+  const rank = rating(voyage,history.slice(0,16))
+  t.is('A',rank)
+});

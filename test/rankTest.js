ced11942f5d9bd6225e4638b26d159be4e8f3c8b
ZHOUGR2 <286348794@qq.com>
2020-08-28 11:35:23 +0800

    [deliveryTest] [Green] fix: fix rankTest file and printTest file

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,113 @@
 const rankTest = require('ava');
+const {rating} = require('../src/rank')
 
-rankTest('foo', t => {
-  t.pass();
+
+rankTest('case 1', t => {
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+
+  const rank = rating(voyage,history.slice(0,10))
+  t.is('A',rank)
+});
+rankTest('case 2', t => {
+    const voyage = {
+      zone: 'east-indies',
+      length: 18,
+    };
+  
+    const rank = rating(voyage,history.slice(0,18))
+    t.is('B',rank)
+});
+rankTest('case 3', t => {
+    const voyage = {
+      zone: 'east-indies',
+      length: 2,
+    };
+  
+    const rank = rating(voyage,history.slice(0,18))
+    t.is('A',rank)
 });
 
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-});
+  
+const voyage = {
+  zone: 'west-indies',
+  length: 10,
+};
+const history = [
+  {
+    zone: 'east-indies',
+    profit: 5,
+  },{
+    zone: 'west-indies',
+    profit: 15,
+  },{
+    zone: 'china',
+    profit: -2,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },  
+  {
+    zone: 'west-africa',
+    profit: 7,
+  }
+];
